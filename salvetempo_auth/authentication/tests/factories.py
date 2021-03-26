906ca6d50aebb1b5c0407162c33a9de857170899
edugarcia98 <edugarcia758@gmail.com>
2021-03-25 22:33:50 -0300

    Add unit tests

---
+++ salvetempo_auth/authentication/tests/factories.py
@@ -0,0 +1,11 @@
+from factory.django import DjangoModelFactory
+
+from ..models import User
+
+
+class UserFactory(DjangoModelFactory):
+    class Meta:
+        model = User
+        django_get_or_create = ("email",)
+
+    email = "test@test.com"
